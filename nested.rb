abc925ced7c7b185afa1831c3f996893cd48576e
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-18 14:28:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -109,7 +109,7 @@ def changing_alan
      alans_new_info = "GUI"
      alan = programmer_hash[:alan_kay]
      alan[:known_for] = "GUI"
-     print alan
+     return alan
 end
 
 def adding_to_dennis
