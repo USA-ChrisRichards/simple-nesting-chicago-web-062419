8a26a8da5ad8c909b1372087b20bd6d42598e5e3
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-18 14:24:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -108,8 +108,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      alan = programmer_hash[:alan_kay]
-     new_alan = alan[:known_for]
-     dictionary['cat'] = "fluffy animal"
+     dictionary[:known_for] = "fluffy animal"
 
 end
 
